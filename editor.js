c9e2307ce3b5621a6f88a6b77c576601beab2d52
Pittab <120305267+Pittab@users.noreply.github.com>
2024-05-15 18:44:28 +0100

    Attempting to fix things

--- editor.js
+++ editor.js
@@ -1,47 +1,78 @@
-var editor="placeholder";
+var editor = "placeholder";
 
-document.addEventListener("DOMContentLoaded", function(){
-    editor = ace.edit("editor");
-    var theme=getCookie("theme");
-    changelang();
-    themechange(theme);
-})
+document.addEventListener("DOMContentLoaded", function () {
+  initialise(getCookie("theme"));
+});
 
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
-function themechange(string){
-    console.log("setting cookie...");
-    setCookie("theme", string, 365);
-    console.log("success!");
-    console.log("changing css..");
-    document.getElementById("theme").href = string + ".css";
-    console.log("success! Changing ace instance theme")
-    editor.setTheme("ace/theme/" + string);
-    console.log("success!");
+function themechange(string) {
+  console.log("setting theme cookie...");
+  setCookie("theme", string, 365);
+  console.log("success!");
+  if (
+    confirm(
+      "Editor will be restarted, are you ok with that? (make sure you save your work)",
+    )
+  ) {
+    editor = "";
+    openTab("null", "Tab1");
+    initialise();
+  } else {
+    alert("Next time you reload CSCode, your changes will be applied");
+  }
 }
 
-function changelang(){
-    var usin=prompt("what language would you like to use?");
-    editor.session.setMode("ace/mode/" + usin); 
+function changelang() {
+  var usin = prompt("what language would you like to use?");
+  editor.session.setMode("ace/mode/" + usin);
+}
+
+function initialise(string) {
+  var editor = ace.edit("editor");
+  console.log("changing css..");
+  document.getElementById("theme").href = string + ".css";
+  console.log("success! Changing ace instance theme");
+  editor.setTheme("ace/theme/" + string);
+  console.log("success!");
+  openTab("null", "Tab1");
+  changelang();
+}
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
