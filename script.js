f8031eb694b1091ee8a5d076b199496d25491e6e
Peter Rimmer <120305267+Pittab@users.noreply.github.com>
2024-05-17 10:25:39 +0000

    imma try this

--- script.js
+++ script.js
@@ -76,4 +76,17 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
+function dl(){
+  console.log("dlSave requested");
+  let textFile = null;
+  const makeTextFile = (text) => {
+    const data = new Blob ([text], { type: 'text/plain', });
+    if (textFile !== null) {
+      window.URL.revokeObjectURL (textFile);
+    }
+    textFile = window.URL.createObjectURL (data);
+    return textFile;
+  }
+}
+
 document.getElementById("defaultOpen").click();
