da0739f1b404f4bd87f90277fa52dd80d5a718ea
Pittab <120305267+Pittab@users.noreply.github.com>
2024-05-19 18:48:28 +0100

    more patch

--- script.js
+++ script.js
@@ -49,13 +49,11 @@ function linewraptoggle(){
   if (getCookie("linewrapping")==false){
     editor.session.setUseWrapMode(true);
     setCookie("linewrapping", true, 365);
-    console.log(getCookie("linewrapping"));
-  } else {
+  } else if (getCookie("linewrapping")==true) {
     editor.session.setUseWrapMode(false);
     setCookie("linewrapping", false, 365);
-    console.log(getCookie("linewrapping"));
-
   }
+  console.log(getCookie("linewrapping"));
 }
 
 function initialise(string) {
