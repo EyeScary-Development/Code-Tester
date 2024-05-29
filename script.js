1afdc30b37dff73b819b42f540778f8e55e5e5f4
Wilbur Williams <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-05-29 17:26:51 +0100

    made it so language can be a query URL param

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ let editor = "";
 let lastset = "none";
 document.addEventListener("DOMContentLoaded", function () {
   initialise(getCookie("theme"));
-  changelang();
+  var langpar = getQueryVariable("lang");
+  if (lang != "no"){
+    editor.session.setMode("ace/mode/" + lang.toLowerCase());
+  } else {
+    changelang();
+  }
 });
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+  return "no";
+}
 
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
