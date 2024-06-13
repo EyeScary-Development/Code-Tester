ccce854039355c2e8ea33f2defd1f869d05d8e85
Peter Rimmer <120305267+Pittab@users.noreply.github.com>
2024-06-13 11:27:59 +0100

    console thinging

--- JS Tester/jstester.js
+++ JS Tester/jstester.js
@@ -1,2 +1,2 @@
 let code=localStorage.getItem("torun")
-document.getElementById("forrunning").innerHTML=code.replace(/console\.log/g, 'document.getElementById("console-emulator").innerHTML +=');
+document.getElementById("forrunning").innerHTML=code.replace(/console\.log/g, 'document.getElementById("console-emulator").innerHTML += "<br> > " + ');
